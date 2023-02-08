d1f5da177bfa6b3b3baa579cf3b3af8a3fd410f9
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-02-08 21:14:22 +0300

    added GUI

--- geometry game/main.py
+++ geometry game/main.py
@@ -2,8 +2,8 @@
     Create Point Class and
     Create Rectangle Class
 """
-import random
 from random import randint
+import turtle
 
 
 class Point:
@@ -30,30 +30,68 @@ class Rectangle:
         return abs((self.point1.x - self.point2.x) * (self.point1.y - self.point2.y))
 
 
-def main():
+class GuiRectangle(Rectangle):
 
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+
+
+class GuiPoint(Point):
+
+    def draw(self, canvas, size=5, color='red'):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+
+
+def main():
     # Create rectangle object with random points
-    rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
-                          Point(randint(10, 19), randint(10, 19)))
+    gui_rectangle = GuiRectangle(Point(randint(0, 300), randint(0, 300)),
+                                 Point(randint(10, 300), randint(10, 300)))
 
     # Print randomly generated rectangle coordinates
     print("Rectangle Coordinates: ",
-          rectangle.point1.x, ",",
-          rectangle.point1.y, "and",
-          rectangle.point2.x, ",",
-          rectangle.point2.y)
+          gui_rectangle.point1.x, ",",
+          gui_rectangle.point1.y, "and",
+          gui_rectangle.point2.x, ",",
+          gui_rectangle.point2.y)
+
+    # print(gui_rectangle.area())
+    # myturtle = turtle.Turtle()
+    # gui_rectangle.draw(canvas=myturtle)
 
     # Get point and area from user
 
     try:
-        user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+        user_point = GuiPoint(float(input("Guess x: ")), float(input("Guess y: ")))
         user_area = float(input("Guess rectangle area: "))
     except ValueError:
         print("The input must be float")
 
     # Print out the game result
-    print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
-    print("Your area was off by: ", rectangle.area() - user_area)
+    print(gui_rectangle.area())
+
+    print("Your point was inside rectangle: ", user_point.falls_in_rectangle(gui_rectangle))
+    print("Your area was off by: ", gui_rectangle.area() - user_area)
+
+    myturtle = turtle.Turtle()
+
+    gui_rectangle.draw(canvas=myturtle)
+    user_point.draw(canvas=myturtle)
+
+    turtle.done()
+
 
 if __name__ == '__main__':
     main()
