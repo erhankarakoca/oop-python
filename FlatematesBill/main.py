fcbd9cf82038823586683712607bc590ae6b9909
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-02-19 12:19:26 +0300

    added PDF report

--- FlatematesBill/main.py
+++ FlatematesBill/main.py
@@ -2,7 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from fpdf import FPDF
+import webbrowser
 
 class Bill:
     """
@@ -47,16 +48,51 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add image
+        pdf.image("files\house.png", w=30, h=30)
+
+        # Insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatemates Bill", border=0, align="C", ln=1)
+
+        # Insert period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert name and due amount of the flatmate 1
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert name and due amount of the flatmate 2
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+
+        # webbrowser.open('file://' +os.path.realpath(self.filename) -> linux or macos
+        webbrowser.open(self.filename)
+
 
 
 # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-    the_bill = Bill(amount=120, period="March 2021")
+if __name__ == '__main__':
+    the_bill = Bill(amount=120, period="April 2021")
     john = Flatmate(name="John", days_in_house=20)
     marry = Flatmate(name="Marry", days_in_house=25)
 
     print("John pays", john.pays(bill=the_bill, flatmate=marry))
     print("Marry pays", marry.pays(bill=the_bill, flatmate=john))
 
+    pdf_report = PdfReport(filename="Report1.pdf")
+    pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
