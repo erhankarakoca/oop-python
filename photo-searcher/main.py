250e5e0fe7656455dfcb7455e671efbd3290c294
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-04-30 14:21:32 +0300

    get images from wikipedia

--- photo-searcher/main.py
+++ photo-searcher/main.py
@@ -1,9 +1,25 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
 
+import wikipedia
+import requests
+
+Builder.load_file('frontend.kv')
 class FirstScreen(Screen):
     def search_image(self):
-        pass
+        # Get user query iput from TextInput
+        query = self.manager.current_screen.ids.user_query.text
+        # Get wikipedia page and list of image urls
+        page = wikipedia.page(query)
+        image_links = page.images[0]
+        #Download the image
+        req = requests.get(image_links)
+        image_path = "files/image.jpg"
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+        # Set the image in the image widget
+        self.manager.current_screen.ids.img.source = image_path
 
 class RootWidget(ScreenManager):
     pass
