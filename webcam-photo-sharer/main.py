11f91fc5397fd0facea80cc96c6455e628414199
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-05-01 22:47:42 +0300

    add start stop camera and capture

--- webcam-photo-sharer/main.py
+++ webcam-photo-sharer/main.py
@@ -1,3 +1,5 @@
+import time
+
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
@@ -8,13 +10,18 @@ Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
-        pass
-
+        self.ids.camera.play = True
+        self.ids.camera_button.text = "Stop Camera"
+        self.ids.camera.texture = self.ids.camera._camera.texture
     def stop(self):
-        pass
-
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
     def capture(self):
-        pass
+        current_time = time.strftime("%Y%m%d-%H%M%S")
+        filename = "files/" + current_time + ".png"
+        self.ids.camera.export_to_png("filename")
+
 
 class ImageScreen(Screen):
     pass
