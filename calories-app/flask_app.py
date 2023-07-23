649e6deb30c7cc52599cc1ab68fcf171ddeed907
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-07-23 20:43:11 +0300

    calorie app done

---
+++ calories-app/flask_app.py
@@ -0,0 +1,22 @@
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
+from calorie import Calorie
+from temperature import Temperature
+
+app = Flask(__name__)
+
+class HomePage(MethodView):
+    def get(self):
+        return render_template("index.html")
+
+
+class CaloriesFormPage(MethodView):
+    def get(self):
+        calories_form = CaloriesForm()
+        return render_template("calories_form_page.html",
+                               caloriesform=calories_form)
+
+    def post(self):
+        calories_form = CaloriesForm(request.form)
+        temperature = Temperature(country = calories_form.country.data,
+                                  city=calories_form.city.data).get()
