69b5bd533ddd3e88868ad3c51ac369ef15b8e24d
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-07-26 21:15:20 +0300

    implement news feed class

--- automated-mail/main.py
+++ automated-mail/main.py
@@ -2,30 +2,34 @@
 import requests
 from pprint import pprint
 class NewsFeed:
+    """Representing multiple news titles and links as a single string
+    """
+    base_url = "https://newsapi.org/v2/everything?"
+    api_key = "749595485081424d858da6aa003c13c3"
 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, interest, from_date, to_date, language):
+        self.interest = interest
+        self.from_date = from_date
+        self.to_date = to_date
+        self.language = language
 
     def get(self):
-        pass
+        url = f"{self.base_url}" \
+              f"qInTitle={self.interest}&" \
+              f"from={self.from_date}&" \
+              f"to={self.to_date}&" \
+              f"language={self.language}&" \
+              f"apiKey={self.api_key}"
 
+        response = requests.get(url)
+        content = response.json()
+        articles = content["articles"]
 
-url = "https://newsapi.org/v2/everything?"\
-      "q=globalwarming&" \
-      "from=2023-06-26&" \
-      "sortBy=publishedAt&" \
-      "apiKey=749595485081424d858da6aa003c13c3"
+        email_body = " "
+        for article in articles:
+            email_body = email_body + article['title'] + "\n" + article['url'] + "\n\n"
 
-response = requests.get(url)
-content = response.json()
-articles = content["articles"]
+        return email_body
 
-
-# for article in articles:
-#     print(article["title"], article["url"])
-
-email_body = " "
-for article in articles:
-    email_body = email_body + article['title'] + "\n" +article['url'] + "\n\n"
-
-print(email_body)
+news_feed = NewsFeed(interest="nasa", from_date="2023-07-24", to_date="2023-07-25", language="en")
+print(news_feed.get())
