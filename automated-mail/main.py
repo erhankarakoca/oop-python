4c6e0c4f643006f7f380161531e4bb174ae0b6cf
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-07-27 22:56:40 +0300

    add spec to run code at a certain time

--- automated-mail/main.py
+++ automated-mail/main.py
@@ -3,19 +3,25 @@ import pandas
 from news import NewsFeed
 df = pandas.read_excel('people.xlsx')
 import datetime
+import time
 
-print(df)
 
-for index, row in df.iterrows():
-    today = datetime.datetime.now().strftime('%Y-%m-%d')
-    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-    news_feed = NewsFeed(interest=row['interest'],
-                         from_date= yesterday,
-                         to_date=today)
-    print(row['email'])
-    email = yagmail.SMTP(user="developertools.cool@gmail.com", password="pxhlbhwbbzthodrb")
+while True:
+    if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 51:
+        print('Executing!')
 
-    email.send(to=row["email"],
-               subject=f"Your {row['interest']} news for today! ",
-               contents=f"Hi, {row['name']}\ See what's on about {rpw['interest']} today. {news_feed.get()}",
-               attachments="design.txt")
+    for index, row in df.iterrows():
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+        news_feed = NewsFeed(interest=row['interest'],
+                             from_date= yesterday,
+                             to_date=today)
+        print(row['email'])
+        email = yagmail.SMTP(user="developertools.cool@gmail.com", password="pxhlbhwbbzthodrb")
+
+        email.send(to=row["email"],
+                   subject=f"Your {row['interest']} news for today! ",
+                   contents=f"Hi, {row['name']}\ See what's on about {row['interest']} today. \n{news_feed.get()}",
+                   attachments="design.txt")
+
+        time.sleep(60)
