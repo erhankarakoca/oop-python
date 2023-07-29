97cfff1c13408feb0a467c3e7771a6682a96c169
Erhan Karakoca <64256175+erhankarakoca@users.noreply.github.com>
2023-07-29 11:41:40 +0300

    refactor the code

--- automated-mail/main.py
+++ automated-mail/main.py
@@ -6,22 +6,25 @@ import datetime
 import time
 
 
+def send_email():
+    today = datetime.datetime.now().strftime('%Y-%m-%d')
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+    news_feed = NewsFeed(interest=row['interest'],
+                         from_date=yesterday,
+                         to_date=today)
+    print(row['email'])
+    email = yagmail.SMTP(user="developertools.cool@gmail.com", password="pxhlbhwbbzthodrb")
+    email.send(to=row["email"],
+               subject=f"Your {row['interest']} news for today! ",
+               contents=f"Hi, {row['name']}\ See what's on about {row['interest']} today. \n{news_feed.get()}",
+               attachments="design.txt")
+
+
 while True:
     if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 51:
         print('Executing!')
 
     for index, row in df.iterrows():
-        today = datetime.datetime.now().strftime('%Y-%m-%d')
-        yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
-        news_feed = NewsFeed(interest=row['interest'],
-                             from_date= yesterday,
-                             to_date=today)
-        print(row['email'])
-        email = yagmail.SMTP(user="developertools.cool@gmail.com", password="pxhlbhwbbzthodrb")
-
-        email.send(to=row["email"],
-                   subject=f"Your {row['interest']} news for today! ",
-                   contents=f"Hi, {row['name']}\ See what's on about {row['interest']} today. \n{news_feed.get()}",
-                   attachments="design.txt")
+        send_email()
 
         time.sleep(60)
